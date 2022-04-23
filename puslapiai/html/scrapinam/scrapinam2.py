ce3942b2958cc282fb00ad6d6c9da60eaea11857
Lithuania54 <edas.simutis@gmail.com>
2022-04-24 01:23:53 +0300

    hmm

--- puslapiai/html/scrapinam/scrapinam2.py
+++ puslapiai/html/scrapinam/scrapinam2.py
@@ -1,5 +1,10 @@
+from gettext import find
+from lib2to3.pgen2 import driver
+from operator import index
+from select import select
 import selenium
 from selenium import webdriver 
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC 
@@ -9,8 +14,11 @@ import json
 option = webdriver.ChromeOptions()
 option.add_argument("--muted")
 
-browser = webdriver.Chrome(options=option, executable_path="C:\\Users\\Rytis\\Projektukas\\scrapinam\\chromedriver.exe")  
+browser = webdriver.Chrome(options=option, executable_path="C:\\xampp\\htdocs\\Projektukas\\puslapiai\\html\\scrapinam\\chromedriver.exe")  
 browser.get("https://lkl.lt/statistika")
+
+search = browser.find_element(By.XPATH, "//*[@id=\"players-stats-widget\"]/div/div[1]/div[1]/div[1]/div/div[1]/select")
+select = browser.find_element(By.XPATH, "//*[@id=\"players-stats-widget\"]/div/div[1]/div[1]/div[1]/div/div[1]/select/option[2]")
 
 arr = []
 lentele = browser.find_element(By.XPATH, "//*[@id=\"players-stats-widget\"]/div/div[2]/div[2]/ul")
@@ -29,14 +37,17 @@ for url in urlarr:
         table = browser.find_element(By.XPATH, "//*[@id=\"players-stats-widget\"]/div/div[2]/div[1]/table/tbody")
 
         for row in table.find_elements(By.XPATH,".//tr"):
-            temparr =[]
-            for cell in row.find_elements(By.XPATH,".//td"): 
-                temparr.append(cell.text)
+            temparr = {}
+            pavadinimai = ['Rankas', 'Zaidejas', "Klubas",  "Rungtynes", "Suma", "Vidurkis"]
+            i = 0 
+            for cell in row.find_elements(By.XPATH,".//td"):     
+                temparr[pavadinimai[i]]=cell.text
+                i +=1
             arr.append(temparr)
     except: 
         print("err")
 
 print(arr)
-file_json = open("scrapinam\\Zaidejai.json", 'w+', encoding='utf-8')
+file_json = open("puslapiai\\html\\scrapinam\\zaidejai2.json", 'w+', encoding='utf-8')
 
 json.dump(arr, file_json, indent = 6, ensure_ascii=False)
