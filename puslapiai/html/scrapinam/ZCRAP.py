2294268fcae483ac74084ba69ead196a89cf01dd
Lithuania54 <edas.simutis@gmail.com>
2022-05-08 01:13:42 +0300

    aha

--- puslapiai/html/scrapinam/ZCRAP.py
+++ puslapiai/html/scrapinam/ZCRAP.py
@@ -14,33 +14,37 @@ option.add_argument("--muted")
 browser = webdriver.Chrome(options=option, executable_path="C:\\xampp\\htdocs\\Projektukas\\puslapiai\\html\\scrapinam\\chromedriver.exe")  
 browser.get("https://lkl.lt/statistika")
 
+mygtukas = browser.find_element(By.XPATH, "/html/body/div[1]/main/div[1]/div/div[2]/div[2]/div/div/div[1]/div[1]/div[1]/div/div[1]/select/option[2]")
+mygtukas.click()
+WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id=\"players-stats-widget\"]/div/div[2]/div[2]/ul")))
+
 arr = []
 lentele = browser.find_element(By.XPATH, "//*[@id=\"players-stats-widget\"]/div/div[2]/div[2]/ul")
+
 
 urlarr  = []
 for lentelesNr in lentele.find_elements(By.XPATH, ".//li"): 
     url = lentelesNr.find_element(By.XPATH, ".//a").get_attribute("href")  
     urlarr.append(url)
 
-for url in urlarr:
-    print(url)
-    try:
-        browser.get(url)
-        WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH,"//*[@id=\"players-stats-widget\"]/div/div[2]/div[1]/table/tbody")))
+print(urlarr)
 
-        table = browser.find_element(By.XPATH, "//*[@id=\"players-stats-widget\"]/div/div[2]/div[1]/table/tbody")
+for index in range(1, 6):
+    browser.get("https://lkl.lt/get-players-stats?category=points&tab=avg&season_id=30527&additional_filters=0&team_id=-&month=&search_text=&page="+str(index))
 
-        for row in table.find_elements(By.XPATH,".//tr"):
-            temparr = {}
-            pavadinimai = ['Rankas', 'Zaidejas', "Klubas",  "Rungtynes", "Suma", "Vidurkis"]
-            i = 0 
-            for cell in row.find_elements(By.XPATH,".//td"):     
-                temparr[pavadinimai[i]]=cell.text
-                i +=1
-            arr.append(temparr)
-    except: 
-        print("err")
+    WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH,"/html/body/div/div[2]/div[1]/table/tbody")))
 
+    table = browser.find_element(By.XPATH, "/html/body/div/div[2]/div[1]/table/tbody")
+
+    for row in table.find_elements(By.XPATH,".//tr"):
+        temparr = {}
+        pavadinimai = ['Rankas', 'Zaidejas', "Klubas",  "Rungtynes", "Suma", "Vidurkis"]
+        i = 0 
+        for cell in row.find_elements(By.XPATH,".//td"): 
+            temparr[pavadinimai[i]]=cell.text
+            i +=1
+        arr.append(temparr)
+ 
 print(arr)
 file_json = open("puslapiai\\html\\scrapinam\\TASKAI.json", 'w+', encoding='utf-8')
 
@@ -85,7 +89,6 @@ for index in range(1, 6):
             i +=1
         arr.append(temparr)
  
-
 print(arr)
 file_json = open("puslapiai\\html\\scrapinam\\KAMUOLIAI.json", 'w+', encoding='utf-8')
 
@@ -116,9 +119,11 @@ for index in range(1, 2):
                 browser.execute_script("window.open('');")
                 browser.switch_to.window(browser.window_handles[1])
                 browser.get(url)
-                WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH,"/html/body/div[1]/main/div[1]/div/div[1]/div[1]/div[1]/img")))
+                WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.XPATH,"/html/body/div[1]/main/div[1]/div/div[1]/div[1]/div[1]/img")))
                 img = browser.find_element(By.XPATH, "/html/body/div[1]/main/div[1]/div/div[1]/div[1]/div[1]/img")
                 temparr["Nuotrauka"] = img.get_attribute("src")
+                img2 = browser.find_element(By.XPATH, "//*[@id=\"app\"]/main/div[1]/div/div[1]/div[1]/div[3]/img")
+                temparr["Nuotrauka2"] = img2.get_attribute("src")
                 browser.close()
                 browser.switch_to.window(browser.window_handles[0])
 
